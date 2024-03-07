ee021705d71d127e5d162e84e4d9557af39c8f8a
danieltk76 <knight.daniel0612@gmail.com>
2024-03-07 17:11:28 -0500

    started springboot stuff and began variable declarations to begin logic for login and password

---
+++ LoginController.java
@@ -0,0 +1,16 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    @PostMapping("/login")
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password) 
+    {
+        
+
+        return "redirect:/home";
+    }
+
+}
